24a18efbde70c548a9ec46a4d83d4109b9dca821
Daniel Maslowski <daniel.maslowski@shop.co>
2017-04-25 00:58:54 +0200

    Enzyme and snapshot tests

--- demo/jest/__tests__/KittyCardSpec.js
+++ demo/jest/__tests__/KittyCardSpec.js
@@ -1,6 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import ReactDOM from 'react-dom';
-import TestUtils from 'react-addons-test-utils';
+import renderer from 'react-test-renderer';
+import { mount as renderFull, shallow as renderShallow } from 'enzyme';
 import KittyCard from '../components/KittyCard/KittyCard';
 
 jest.unmock('../components/KittyCard/KittyCard');
@@ -9,19 +11,24 @@ jest.unmock('../components/KittyCard/KittyCard');
 const kitty = { _id: 123, name: 'Miss Kitty Fantastico' };
 
 describe('KittyCard', () => {
+  it('renders correctly', () => {
+    const kittyCard = renderer.create(
+      <KittyCard kitty={kitty} />
+    ).toJSON();
+    expect(kittyCard).toMatchSnapshot();
+  });
+
   it('shows the name of the cat', () => {
-    const kittyCard = TestUtils.renderIntoDocument(
+    const kittyCard = renderShallow(
       <KittyCard kitty={kitty} />
     );
-    const renderedKittyCard = ReactDOM.findDOMNode(kittyCard);
-    expect(renderedKittyCard.textContent).toMatch(/Miss Kitty Fantastico/);
+    expect(kittyCard.text()).toMatch(/Miss Kitty Fantastico/);
   });
 
   it('shows the number of the cat', () => {
-    const kittyCard = TestUtils.renderIntoDocument(
+    const kittyCard = renderFull(
       <KittyCard kitty={kitty} />
     );
-    const renderedKittyCard = ReactDOM.findDOMNode(kittyCard);
-    expect(renderedKittyCard.textContent).toMatch(/is cat #123/);
+    expect(kittyCard.text()).toMatch(/is cat #123/);
   });
 });
