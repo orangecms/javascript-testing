37fa55de420fee3942ed73949a3aa619c087d6d1
Daniel Maslowski <daniel.maslowski@shop.co>
2017-04-26 10:05:55 +0200

    fix indentation in Jasmine demo

--- demo/jasmine/spec/rngSpec.js
+++ demo/jasmine/spec/rngSpec.js
@@ -6,11 +6,11 @@ describe('rng', () => {
   it('should return a number', () => {
     const random1 = rng();
     expect(random1).toBeNumber();
-});
+  });
 
   it('should return a random number', () => {
     const random1 = rng();
     const random2 = rng();
     expect(random1).not.toEqual(random2);
-});
+  });
 });
